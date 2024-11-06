659f819bdcf2a39197d243e9cdd2f0eb575249f1
Adú <adu.matory@gmail.com>
2024-11-06 08:08:34 +0000

    restructure main.py for clarity; remove file clutter

--- scripts/main.py
+++ scripts/main.py
@@ -117,7 +117,7 @@ def add_main_args(parser: LightningArgumentParser) -> LightningArgumentParser:
     parser.add_argument(
         "--num_workers",
         type=int,
-        default=0,
+        default=1,
         help="Number of processes to running in parallel"
     )
 
@@ -147,6 +147,87 @@ def get_datamodule_num_workers(num_process_workers=None):
         datamodule_num_workers = math.floor(n_cpus/num_process_workers * .9) 
     return datamodule_num_workers
 
+def get_datamodule(args):
+    # get workers for datamodule
+    datamodule_num_workers = get_datamodule_num_workers(args.num_workers)
+    
+    # get datamodule args
+    print(args)
+    datamodule_vars = vars(args[args.dataset_name])
+    update_vars = {k:v for k,v in vars(args).items() if k in datamodule_vars}
+    datamodule_vars.update(update_vars)
+    datamodule_vars.update({'num_workers': datamodule_num_workers})
+
+    # init data module
+    datamodule = NAME_TO_DATASET_CLASS[args.dataset_name](**datamodule_vars)
+
+    return datamodule
+
+def get_model(args):
+    print(f"Initializing {args.model_name} model")
+    if args.checkpoint_path is None:
+        model_vars = vars(args[args.model_name])
+        update_vars = {k:v for k,v in vars(args).items() if k in model_vars}
+        model_vars.update(update_vars)
+        print('with params ', model_vars)
+        model = NAME_TO_MODEL_CLASS[args.model_name](**model_vars)
+    else:
+        model = NAME_TO_MODEL_CLASS[args.model_name].load_from_checkpoint(args.checkpoint_path)
+
+    return model
+
+def get_trainer(args, logger=None, callbacks=[]):
+    args.trainer.accelerator = 'auto'
+    args.trainer.strategy = 'ddp'
+    args.trainer.logger = logger
+    args.trainer.precision = "bf16-mixed" ## This mixed precision training is highly recommended
+    args.trainer.min_epochs = 100
+
+    # set checkpoint save directory
+    dirpath = os.path.join(dirname, '../models', args.model_name)
+    if not os.path.isdir(dirpath):
+        os.makedirs(dirpath)
+
+    args.trainer.callbacks = callbacks
+
+    # init trainer
+    trainer_args = vars(args.trainer)
+    trainer = pl.Trainer(**trainer_args)
+
+    return trainer
+
+def get_logger(args):
+    logger = pl.loggers.WandbLogger(project=args.project_name,
+                                    entity=args.wandb_entity,
+                                    group=args.model_name,
+                                    dir=os.path.join(dirname, '..'))
+
+    return logger
+
+def get_callbacks(args):
+    # set checkpoint save directory
+    dirpath = os.path.join(dirname, '../models', args.model_name)
+    if not os.path.isdir(dirpath):
+        os.makedirs(dirpath)
+    
+    callbacks = [
+        pl.callbacks.ModelCheckpoint(
+            monitor=args.monitor_key,
+            mode='min' if "loss" in args.monitor_key else "max",
+            dirpath=dirpath,
+            filename=args.model_name + '-{epoch:002d}-{val_loss:.2f}',
+            save_last=True
+        ),
+        pl.callbacks.EarlyStopping(
+            monitor=args.monitor_key,
+            mode='min' if "loss" in args.monitor_key else "max",
+            patience=10,
+            check_on_train_epoch_end=True
+        )]
+    
+    return callbacks
+
+
 def main(args: argparse.Namespace):
     print("Loading data ..")
     print(f"Training: {args.train}")
@@ -161,63 +242,12 @@ def main(args: argparse.Namespace):
         However, you may want to alter this code for special localization logic or to suit your risk
         model implementations
     """
-    # get workers for datamodule
-    datamodule_num_workers = get_datamodule_num_workers(args.num_workers)
-    
-    # get datamodule args
-    datamodule_vars = vars(args[args.dataset_name])
-    update_vars = {k:v for k,v in vars(args).items() if k in datamodule_vars}
-    datamodule_vars.update(update_vars)
-    datamodule_vars.update({'num_workers': datamodule_num_workers})
 
-    # init data module
-    datamodule = NAME_TO_DATASET_CLASS[args.dataset_name](**datamodule_vars)
-
-    print(f"Initializing {args.model_name} model")
-    if args.checkpoint_path is None:
-        model_vars = vars(args[args.model_name])
-        update_vars = {k:v for k,v in vars(args).items() if k in model_vars}
-        model_vars.update(update_vars)
-        print('with params ', model_vars)
-        model = NAME_TO_MODEL_CLASS[args.model_name](**model_vars)
-    else:
-        model = NAME_TO_MODEL_CLASS[args.model_name].load_from_checkpoint(args.checkpoint_path)
-
-    print("Initializing trainer")
-    logger = pl.loggers.WandbLogger(project=args.project_name,
-                                    entity=args.wandb_entity,
-                                    group=args.model_name,
-                                    dir=os.path.join(dirname, '..'))
-
-    args.trainer.accelerator = 'auto'
-    args.trainer.strategy = 'ddp'
-    args.trainer.logger = logger
-    args.trainer.precision = "bf16-mixed" ## This mixed precision training is highly recommended
-    args.trainer.min_epochs = 200
-
-    # set checkpoint save directory
-    dirpath = os.path.join(dirname, '../models', args.model_name)
-    if not os.path.isdir(dirpath):
-        os.makedirs(dirpath)
-
-    args.trainer.callbacks = [
-        pl.callbacks.ModelCheckpoint(
-            monitor=args.monitor_key,
-            mode='min' if "loss" in args.monitor_key else "max",
-            dirpath=dirpath,
-            filename=args.model_name + '-{epoch:002d}-{val_loss:.2f}',
-            save_last=True
-        ),
-        pl.callbacks.EarlyStopping(
-            monitor=args.monitor_key,
-            mode='min' if "loss" in args.monitor_key else "max",
-            patience=10,
-            check_on_train_epoch_end=True
-        )]
-
-    # init trainer
-    trainer_args = vars(args.trainer)
-    trainer = pl.Trainer(**trainer_args)
+    datamodule = get_datamodule(args)
+    model = get_model(args)
+    logger = get_logger(args)
+    callbacks = get_callbacks(args)
+    trainer = get_trainer(args, callbacks=callbacks, logger=logger)
 
     if args.train:
         print("Training model")
@@ -231,7 +261,8 @@ def main(args: argparse.Namespace):
     print("Evaluating model on test set")
     trainer.test(model, datamodule)
 
-    logger.finalize('success')
+    if logger:
+        logger.finalize('success')
     wandb.finish()
 
     print("Done")

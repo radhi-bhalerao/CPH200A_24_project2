d6c5a907f07cd6a616e82ca29ff08e28b01ff9f4
Adú <adu.matory@gmail.com>
2024-11-27 00:16:34 +0000

    add risk model training command

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,3 +2,6 @@
 
 # run grid search for part 1.1
 python scripts/dispatcher1.py
+
+# train risk model (with resnet3d backbone) for part 3.2
+python scripts/main.py --risk --model resnet3d --train True --dataset_name nlst --batch_size 5 --pretraining True --class_balance True --max_followup 6
